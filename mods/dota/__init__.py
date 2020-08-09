4ad7fce21fb502da0c905c2182e4ab5c14dbe8ee
tang yifeng <yifengtang_hust@outlook.com>
2020-08-09 22:34:46 +0800

    rebuild helper and 24hrs games

--- mods/dota/__init__.py
+++ mods/dota/__init__.py
@@ -1,30 +1,34 @@
 #encoding=Utf-8
 from mirai import Mirai, Group, GroupMessage, MessageChain, Member, Plain, Image, Face, AtAll, At,FlashImage, exceptions
-
 from .helper import readDict, updateDict, getDotaInfo
+from .games_24hrs import getGamesIn24Hrs
 
-sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
+# sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
+# dota_id_dict = readDict()
+#
+#
+# @sub_app.receiver("GroupMessage")
+# async def dota_handler(app: Mirai, group:Group, message:MessageChain, member:Member):
+#     if message.toString()[:5] == "/dota":
+#         query_id = message.toString()[6:]
+#         if query_id in dota_id_dict.keys():
+#             query_id = dota_id_dict[query_id]
+#         res = getGamesIn24Hrs(query_id)
+#         msg = [Plain(text=res)]
+#         try:
+#             await app.sendGroupMessage(group,msg)
+#         except exceptions.BotMutedError:
+#             pass
+#     elif message.toString()[:8] == "/setdota":
+#         rec = message.toString()[9:].split(" ")
+#         dota_id_dict[rec[0]] = rec[1]
+#         updateDict(dota_id_dict)
+#         msg = [Plain(text="添加成功！")]
+#         try:
+#             await app.sendGroupMessage(group,msg)
+#         except exceptions.BotMutedError:
+#             pass
 
-dota_id_dict = readDict()
 
-@sub_app.receiver("GroupMessage")
-async def dota_handler(app: Mirai, group:Group, message:MessageChain, member:Member):
-    if message.toString()[:5] == "/dota":
-        query_id = message.toString()[6:]
-        if query_id in dota_id_dict.keys():
-            query_id = dota_id_dict[query_id]
-        res = getDotaInfo(query_id)
-        msg = [Plain(text=res)]
-        try:
-            await app.sendGroupMessage(group,msg)
-        except exceptions.BotMutedError:
-            pass
-    elif message.toString()[:8] == "/setdota":
-        rec = message.toString()[9:].split(" ")
-        dota_id_dict[rec[0]] = rec[1]
-        updateDict(dota_id_dict)
-        msg = [Plain(text="添加成功！")]
-        try:
-            await app.sendGroupMessage(group,msg)
-        except exceptions.BotMutedError:
-            pass
+if __name__ == "__main__":
+    print(getGamesIn24Hrs("dasf"))
