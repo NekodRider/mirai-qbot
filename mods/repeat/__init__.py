e78ebf1e348d3592deb96696e6b86152dad58cb8
NekodRider <driver2223@gmail.com>
2020-08-10 17:28:05 +0800

    fix command repeat and type error

--- mods/repeat/__init__.py
+++ mods/repeat/__init__.py
@@ -33,7 +33,7 @@ async def repeat_handler(app: Mirai, group:Group, message:MessageChain, member:M
     sender=member.id
     groupId=group.id
     global repeat_queue
-    if message.toString() == repeat_queue[0] and message.toString() != repeat_log[0] and sender != repeat_queue[1]:
+    if message.toString()[0]!='/' and message.toString() == repeat_queue[0] and message.toString() != repeat_log[0] and sender != repeat_queue[1]:
         SessionLogger.info("[REPEAT]来自群%d中成员%d的消息:" % (groupId,sender) + message.toString())
         try:
             msg = stringToMsg(message.toString())
