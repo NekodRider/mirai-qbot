3c2fc2a767452de9d73cb765f554ab4f0545f41d
NekodRider <driver2223@gmail.com>
2020-08-10 14:21:38 +0800

    add logger for mods

--- mods/repeat/__init__.py
+++ mods/repeat/__init__.py
@@ -29,8 +29,10 @@ def stringToMsg(s):
 
 @sub_app.receiver("GroupMessage")
 async def repeat_handler(app: Mirai, group:Group, message:MessageChain, member:Member):
-    sender = member.id
+    sender=member.id
+    groupId=group.id
     if message.toString() == repeat_queue[0] and message.toString() != repeat_log[0] and sender != repeat_queue[1]:
+        SessionLogger.info("[REPEAT]来自群%d中成员%d的消息:" % (groupId,sender),message.toString())
         try:
             msg = stringToMsg(message.toString())
             await app.sendGroupMessage(group,msg)
