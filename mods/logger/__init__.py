90f6f8b46973ddf2eb3a5b0b9f736451342e5fe3
NekodRider <driver2223@gmail.com>
2020-08-10 17:43:55 +0800

    change log path

--- mods/logger/__init__.py
+++ mods/logger/__init__.py
@@ -1,5 +1,6 @@
 from mirai import Mirai, Group, FriendMessage, GroupMessage, MessageChain, Member, Plain, Image, Face, AtAll, At,FlashImage, exceptions
 from mirai.logger import Session as SessionLogger
+from pathlib import Path
 
 sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
 
@@ -8,7 +9,7 @@ sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
 async def logger_handler(app: Mirai, sender: "Sender", event_type: "Type", message: MessageChain):
     if message.toString() == "/log":
         SessionLogger.info("[LOGGER]来自群%d中成员%d的消息:" % (sender.group.id,sender.id) + message.toString())
-        with open("/var/log/mirai-qbot.log","r") as f:
+        with open(Path(__file__).parent.parent.parent.joinpath("mirai-qbot.log"),"r") as f:
             res = f.readlines()
             res = "".join(res[0 if len(res)<20 else len(res)-20:])
         try:
