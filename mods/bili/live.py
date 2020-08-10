3f0a955fb94ccf3f6ce0679d1d26aa840879953d
NekodRider <driver2223@gmail.com>
2020-08-11 06:14:05 +0800

    add live status

---
+++ mods/bili/live.py
@@ -0,0 +1,22 @@
+from urllib import request
+import json
+
+def getLiveInfo(room_id):
+    url = "https://api.live.bilibili.com/room/v1/Room/get_info?id=" + room_id
+    html = request.urlopen(url)
+    live_data = json.loads(html.read().decode('utf-8'))
+    if live_data["code"]!=0:
+        return "error"
+    uid = live_data["data"]["uid"]
+    res = {}
+    res['isLive'] = live_data["data"]["live_status"]
+    res['title'] = live_data["data"]["title"]
+    res['keyframe'] = live_data["data"]["keyframe"]
+    res['area_name'] = live_data["data"]["area_name"]
+    res['url'] = "https://live.bilibili.com/" + room_id
+    url = "https://api.bilibili.com/x/space/acc/info?mid=" + uid
+    html = request.urlopen(url)
+    live_data = json.loads(html.read().decode('utf-8'))
+    res['name'] = live_data["data"]["name"]
+
+    return res
