1d828f3db3a1d12c75eab93660be4c92f2f5b927
tang yifeng <yifengtang_hust@outlook.com>
2020-08-10 23:52:59 +0800

    add latest games stat

--- mods/dota/__init__.py
+++ mods/dota/__init__.py
@@ -4,6 +4,7 @@ from mirai.logger import Session as SessionLogger
 from .helper import readDict, updateDict, getDotaPlayerInfo, getDotaGamesInfo, error_codes
 from .games_24hrs import getGamesIn24Hrs
 from .winning_rate import getWinningRateGraph
+from .latest_games import getLatestWinningStat
 from pathlib import Path
 
 sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
@@ -27,6 +28,29 @@ async def dota_handler(app: Mirai, group:Group, message:MessageChain, member:Mem
             SessionLogger.info("[DOTA]返回成功")
         try:
             await app.sendGroupMessage(group,msg)
+        except exceptions.BotMutedError:
+            pass
+    elif message.toString()[:5] == "/stat":
+        SessionLogger.info("[STAT]来自群%d中成员%d的消息:" % (groupId,sender) + message.toString())
+        query_id = message.toString()[6:].split(" ")
+        if query_id[0] not in dota_id_dict.keys():
+            msg = [Plain(text="未添加该用户！")]
+            SessionLogger.info("[STAT]未添加该用户")
+        else:
+            query_id[0] = dota_id_dict[query_id[0]]
+            args = 20
+            if len(query_id) == 2:
+                try:
+                    args = int(query_id[1])
+                    if args > 50 or args <= 0:
+                        args = 20
+                except ValueError:
+                    args = 20
+            res = getLatestWinningStat(query_id[0], args)
+            msg = [Plain(text=res)]
+            SessionLogger.info("[STAT]返回成功")
+        try:
+            await app.sendGroupMessage(group, msg)
         except exceptions.BotMutedError:
             pass
     elif message.toString()[:8] == "/winrate":
