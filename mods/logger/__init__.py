2e3015d77ee679188d33b0ad7fc4316ab81789a7
NekodRider <driver2223@gmail.com>
2020-08-11 06:33:16 +0800

    rm DM logger and fix live

--- mods/logger/__init__.py
+++ mods/logger/__init__.py
@@ -4,22 +4,15 @@ from pathlib import Path
 
 sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
 
-@sub_app.receiver(FriendMessage)
 @sub_app.receiver(GroupMessage)
 async def logger_handler(app: Mirai, sender: "Sender", event_type: "Type", message: MessageChain):
     if message.toString() == "/log":
-        if event_type == "GroupMessage":
-            SessionLogger.info("[LOGGER]来自群%d中成员%d的消息:" % (sender.group.id,sender.id) + message.toString())
-        elif event_type == "FriendMessage":
-            SessionLogger.info("[LOGGER]来自成员%d的消息:" % (sender.id) + message.toString())
+        SessionLogger.info("[LOGGER]来自群%d中成员%d的消息:" % (sender.group.id,sender.id) + message.toString())
         with open(Path(__file__).parent.parent.parent.joinpath("mirai-qbot.log"),"r") as f:
             res = f.readlines()
             res = "".join(res[0 if len(res)<20 else len(res)-20:])
         try:
             msg = [Plain(text=res)]
-            if event_type == "GroupMessage":
-                await app.sendGroupMessage(sender.group, msg)
-            elif event_type == "FriendMessage":
-                await app.sendFriendMessage(sender, msg)
+            await app.sendGroupMessage(sender.group, msg)
         except:
             pass
