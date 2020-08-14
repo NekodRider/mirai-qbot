4d34ec9c6e25ab302269b096e49664a6fa6f8c30
MMMartt <nielumartin@hotmail.com>
2020-08-14 20:23:53 +0800

    finish sb-repeater

--- mods/repeat/__init__.py
+++ mods/repeat/__init__.py
@@ -3,6 +3,7 @@ from mirai.logger import Session as SessionLogger
 from .._utils import stringToMsg
 from .. import PREFIX
 import random
+import re
 
 sub_app = Mirai(f"mirai://localhost:8080/?authKey=0&qq=0")
 
@@ -14,6 +15,14 @@ async def repeat_handler(app: Mirai, group:Group, message:MessageChain, member:M
     sender=member.id
     groupId=group.id
     global repeat_queue
+    pattern = "^\s*\S{2,6}[SNsn][Bb][!！?？.。]{0,10}\s*$"
+    if re.match(pattern, message.toString()):
+        try:
+            msg = stringToMsg(message.toString())
+            await app.sendGroupMessage(group,msg)
+        except exceptions.BotMutedError:
+            pass
+        return
     if message.toString()[0]!=PREFIX and message.toString() == repeat_queue[0] and message.toString() != repeat_log[0] and sender != repeat_queue[1]:
         SessionLogger.info("[REPEAT]来自群%d中成员%d的消息:" % (groupId,sender) + message.toString())
         try:
