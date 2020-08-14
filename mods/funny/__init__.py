079c89402a1de33a26d625b1cfa8fd9778464383
MMMartt <nielumartin@hotmail.com>
2020-08-14 19:30:05 +0800

    fix lost jrrp changes while rebasing

--- mods/funny/__init__.py
+++ mods/funny/__init__.py
@@ -5,36 +5,39 @@ from .jrrp import calcJrrp
 from ..users import getUserInfo
 
 
-async def jrrp_handler(*args, sender, event_type):
+async def jrrp_handler(*args, sender: Member, event_type):
     """
     return 'YD 今日人品为 99,YDNB！
 
     0 -> YDSB!!!
-    1-20 -> YDSB!!
-    21-50 -> YDSB!
-    51-80 -> YDNB!
-    80-99 -> YDNB!!
+    1-40 -> 'YD不NB，但TDNB！'
+    41-70 -> YDNB!
+    71-99 -> YDNB!!
     100 -> YDNB!!!
     """
     if event_type != "GroupMessage":
         return [Plain(text="尚未支持该类型")]
-    rp = calcJrrp(sender.group.id, sender.id)
+    target = getUserInfo(sender.id)
+
+    nickname = sender.memberName
+    hint = ''
+    if target is None:
+        hint = '\n\n可以通过 /setname 为自己设定名字哦！'
+    else:
+        nickname = target['nickname']
+
+    nickname = nickname.upper()
     msg = '%s今日人品为%d，%s'
+    rp = calcJrrp(sender.group.id, sender.id)
     postfix = 'NB！！！'
     if rp == 0:
         postfix = 'SB!!!!'
-    elif rp < 50:
-        postfix = '不NB，但YDNB！'
-    elif rp < 81:
+    elif rp < 40:
+        postfix = '不NB，但%sNB！' % 'yd' if nickname != 'yd' else 'td'
+    elif rp < 71:
         postfix = 'NB！'
     elif rp < 100:
         postfix = 'NB！！'
-    nickname = sender.memberName.upper()
-    hint = ''
-    try:
-        nickname = getUserInfo(sender.id)['nickname'].upper()
-    except:
-        hint = '\n\n可以通过 /setname 为自己设定名字哦！'
     return [Plain(text=(msg+postfix) % (nickname, rp, nickname) + hint)]
 
 COMMANDS = {"jrrp": jrrp_handler}
