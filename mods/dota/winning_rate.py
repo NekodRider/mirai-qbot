8f6c30d675151884fe34e7eed612bf8aa4194837
NekodRider <driver2223@gmail.com>
2020-08-21 17:54:57 +0800

    add wrcp command

--- mods/dota/winning_rate.py
+++ mods/dota/winning_rate.py
@@ -4,8 +4,7 @@ import numpy as np
 from pathlib import Path
 from functools import reduce
 
-
-def getWinningRateGraph(playerId, total=20):
+def getWinRateList(playerId, total=20):
     res = []
     player_data = getDotaPlayerInfo(playerId, "/summary")
     if type(player_data) == type(""):
@@ -32,7 +31,13 @@ def getWinningRateGraph(playerId, total=20):
         if result == 1:
             pre_win += 1
         winning_rate.append(round(pre_win / pre_total * 100, 4) if (pre_total != 0) else 100)
-    graph_index = range(0, latest_total + 1)
+    return winning_rate, player_name
+
+def getWinRateGraph(playerId, total=20):
+    winning_rate, player_name = getWinRateList(playerId,total)
+    if type(winning_rate) == type(""):
+        return winning_rate, 0
+    graph_index = range(0, total + 1)
     graph_max = np.max(winning_rate)
     graph_min = np.min(winning_rate)
     graph_range = graph_max - graph_min
@@ -47,3 +52,29 @@ def getWinningRateGraph(playerId, total=20):
     pic_name = str(Path(__file__).parent.joinpath(playerId + "_winning_rate.png"))
     plt.savefig(pic_name)
     return pic_name, player_name
+
+def getCompWinRateGraph(playerIdA, playerIdB, total=20):
+    winning_rate_a, player_name_a = getWinRateList(playerIdA,total)
+    winning_rate_b, player_name_b = getWinRateList(playerIdB,total)
+    if type(winning_rate_a) == type(""):
+        return winning_rate_a, 0, 0
+    if type(winning_rate_b) == type(""):
+        return winning_rate_b, 0, 0
+    
+    graph_index = range(0, total + 1)
+    graph_max = np.max(winning_rate_a + winning_rate_b)
+    graph_min = np.min(winning_rate_a + winning_rate_b)
+    graph_range = graph_max - graph_min
+
+    plt.figure()
+    plt.title('Winrate Comparison in the latest ' + str(total) + " games")
+    plt.ylim(graph_min - (graph_range) / 25, graph_max + (graph_range) / 25)
+    plt.plot(graph_index, winning_rate_a, color="red", label=player_name_a + "'s Winrate")
+    plt.plot(graph_index, winning_rate_b, color="blue", label=player_name_b + "'s Winrate")
+    plt.scatter(graph_index, winning_rate_a, color="red", s=15)
+    plt.scatter(graph_index, winning_rate_b, color="blue", s=15)
+    plt.legend()
+    plt.draw()
+    pic_name = str(Path(__file__).parent.joinpath(playerIdA + playerIdB + "_winning_rate.png"))
+    plt.savefig(pic_name)
+    return pic_name, player_name_a, player_name_b
