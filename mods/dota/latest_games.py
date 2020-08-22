3d500afa3a8a35b00c61d4ff976fbc992444147c
NekodRider <driver2223@gmail.com>
2020-08-22 18:34:06 +0800

    fix plot param

--- mods/dota/latest_games.py
+++ mods/dota/latest_games.py
@@ -137,7 +137,7 @@ def getCompStarStat(playerIdA, playerIdB, total=20):
     raw_data_b = getStarScore(reports_b,gpm_b)
 
     fig=plt.figure(figsize=(4, 5))
-    ax1=fig.add_subplot(1,2,1,polar=True)
+    ax1=fig.add_subplot(1,1,1,polar=True)
     ax1.set_title(f'{player_name_a} VS {player_name_b} 最近 {str(total)} 场游戏数据对比')
     ax1.set_rlim(0,10.5)
 
