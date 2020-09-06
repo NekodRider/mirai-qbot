885056ab6b0bb17f2dd75f2c850c493629a6730a
NekodRider <driver2223@gmail.com>
2020-09-06 20:33:12 +0800

    add install script

--- install.py
+++ install.py
@@ -1,17 +1,34 @@
+#coding=utf-8
 import matplotlib
 import os
 import shutil
 from pathlib import Path
 
 font_file = "./resource/wqy-microhei.ttc"
+sys_font_dir = "/usr/share/fonts/truetype"
 # 查找字体路径
-font_path = matplotlib.matplotlib_fname()
-shutil.copyfile(font_file,os.path.join(font_path,"wqy-microhei.ttc"))
-rc_file = Path(font_path).parent.parent.joinpath("mpl-data","matplotlibrc")
-with open(rc_file,"a") as f:
-    f.write("font.family  : sans-serif")
-    f.write("font.sans-serif : WenQuanYi Micro Hei, Arial, Bitstream Vera Sans, Lucida Grande, Verdana, Geneva, Lucid, Helvetica, Avant Garde, sans-serif")
-# 查找字体缓存路径
+rc_file = matplotlib.matplotlib_fname()
+font_path = Path(rc_file).parent.joinpath("fonts","ttf")
+shutil.copyfile(font_file,font_path.joinpath("wqy-microhei.ttc"))
+shutil.copyfile(font_file,Path(sys_font_dir).joinpath("wqy-microhei.ttc"))
+shutil.copy(rc_file,Path(rc_file).parent.joinpath("matplotlibrc.bak"))
+flist = []
+with open(rc_file,"r",encoding='utf-8') as f:
+    flist = f.readlines()
+    a,b = False,False
+    for no,line in enumerate(flist):
+        if not a and "font.family:" in line:
+            a = True
+            flist[no] = "font.family : sans-serif\n"
+        elif not b and "font.sans-serif:" in line:
+            b = True
+            flist[no] = "font.sans-serif : WenQuanYi Micro Hei, Arial, Bitstream Vera Sans, Lucida Grande, Verdana, Geneva, Lucid, Helvetica, Avant Garde, sans-serif\n"
+        if a and b:
+            break
+with open(rc_file,"w",encoding='utf-8') as f:
+    f.writelines(flist)
 font_cache_path = matplotlib.get_cachedir()
 shutil.rmtree(font_cache_path)
 os.system("timedatectl set-timezone Asia/Shanghai")
+os.system("apt install fontconfig")
+os.system("fc-cache -fv")
