9a22dd13707832da676f8db1557751e1e14cb650
NekodRider <driver2223@gmail.com>
2020-09-15 12:33:37 +0800

    fix qqface support

--- mods/_utils/convert.py
+++ mods/_utils/convert.py
@@ -1,8 +1,6 @@
 from mirai import Group, Permission, Image, Face, At, AtAll, FlashImage, Plain
+from mirai.face import QQFaces
 import re
-
-face_list = ["jingya", "piezui", "se", "fadai", "deyi", "liulei", "haixiu", "bizui", "shui", "daku", "ganga", "fanu", "tiaopi", "ciya", "weixiao", "nanguo", "ku", "zhuakuang", "tu", "touxiao", "keai", "baiyan", "aoman", "ji_e", "kun", "jingkong", "liuhan", "hanxiao", "dabing", "fendou", "zhouma", "yiwen", "yun", "zhemo", "shuai", "kulou", "qiaoda", "zaijian", "fadou", "aiqing", "tiaotiao", "zhutou", "yongbao", "dan_gao", "shandian", "zhadan", "dao", "zuqiu", "bianbian", "kafei", "fan", "meigui", "diaoxie", "aixin", "xinsui", "liwu", "taiyang", "yueliang", "qiang", "ruo", "woshou", "shengli", "feiwen", "naohuo", "xigua", "lenghan", "cahan", "koubi", "guzhang", "qiudale", "huaixiao", "zuohengheng", "youhengheng", "haqian", "bishi", "weiqu", "kuaikule", "yinxian", "qinqin", "xia", "kelian", "caidao", "pijiu", "lanqiu", "pingpang", "shiai", "piaochong", "baoquan", "gouyin", "quantou", "chajin", "aini", "bu", "hao", "zhuanquan", "ketou", "huitou", "tiaosheng", "huishou", "jidong", "jiewu",
-             "xianwen", "zuotaiji", "youtaiji", "shuangxi", "bianpao", "denglong", "facai", "K_ge", "gouwu", "youjian", "shuai_qi", "hecai", "qidao", "baojin", "bangbangtang", "he_nai", "xiamian", "xiangjiao", "feiji", "kaiche", "gaotiezuochetou", "chexiang", "gaotieyouchetou", "duoyun", "xiayu", "chaopiao", "xiongmao", "dengpao", "fengche", "naozhong", "dasan", "caiqiu", "zuanjie", "shafa", "zhijin", "yao", "shouqiang", "qingwa", "hexie", "yangtuo", "youling", "dan", "juhua", "hongbao", "daxiao", "bukaixin", "lengmo", "e", "haobang", "baituo", "dianzan", "wuliao", "tuolian", "chi", "songhua", "haipa", "huachi", "xiaoyanger", "biaolei", "wobukan", "bobo", "hulian", "paitou", "cheyiche", "tianyitian", "cengyiceng", "zhuaizhatian", "dingguagua", "baobao", "baoji", "kaiqiang", "liaoyiliao", "paizhuo", "paishou", "gongxi", "ganbei", "chaofeng", "heng", "foxi", "qiaoyiqioa", "jingdai", "chandou", "kentou", "toukan", "shanlian", "yuanliang", "penlian", "shengrikuaile", "touzhuangji", "shuaitou", "rengou"]
 
 
 def groupToStr(g):
@@ -27,10 +25,9 @@ def stringToMsg(s):
     for i in comps:
         if "Image" in i:
             msg.append(Image(imageId=i.split("::")[-1]))
-        # 需要修改Face toString方法
         elif "Face" in i:
             try:
-                msg.append(Face(faceId=face_list.index(i.split("name=")[-1])))
+                msg.append(Face(faceId=QQFaces[i.split("name=")[-1]]))
             except ValueError:
                 continue
         elif "AtAll" in i:
