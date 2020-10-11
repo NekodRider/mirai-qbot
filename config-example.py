b06da3714767321f60820cd23cc8c9e63fc69436
NekodRider <driver2223@gmail.com>
2020-10-12 02:27:48 +0800

    add roll and minor fix

--- config-example.py
+++ config-example.py
@@ -1,9 +1,9 @@
 app_configs = {
-    "account":"88888888",
-    "authKey":"12345678",
-    "host":"http://localhost:23333",
-    "websocket":True
+    "account": "88888888",
+    "authKey": "12345678",
+    "host": "http://localhost:23333",
+    "websocket": True
 }
-configs = {
-    "prefix":"/"
+bot_configs = {
+    "prefix": "/"
 }
--- mods/logger/__init__.py
+++ mods/logger/__init__.py
@@ -1,11 +1,15 @@
 from graia.application.message.chain import MessageChain
 from graia.application.message.elements.internal import Plain
+from graia.application.group import Member
+from graia.application.friend import Friend
 from pathlib import Path
+import typing as T
 
-async def logger_handler(*args, subject):
-    with open(Path(__file__).parent.parent.parent.joinpath("logs","mirai_bot.log"),"r",encoding='utf-8') as f:
+
+async def logger_handler(*args, subject: T.Union[Member, Friend]):
+    with open(Path(__file__).parent.parent.parent.joinpath("logs", "mirai_bot.log"), "r", encoding='utf-8') as f:
         res = f.readlines()
-        res = "".join(res[0 if len(res)<21 else len(res)-21:len(res)-1])
+        res = "".join(res[0 if len(res) < 21 else len(res)-21:len(res)-1])
     return MessageChain.create([Plain(res)])
 
 COMMANDS = {"log": logger_handler}
