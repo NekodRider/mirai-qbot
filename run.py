b15dea42eb2abfb713b95a6bbe96a7c4f8e0c30f
NekodRider <driver2223@gmail.com>
2020-10-14 03:05:35 +0800

    add plus and sum for dnd style roll

--- run.py
+++ run.py
@@ -8,7 +8,5 @@ if __name__ == "__main__":
         print("请根据config-example.py创建config.py！")
         exit(1)
     config = importlib.import_module("config")
-
     bot = Bot(config.app_configs, config.bot_configs)
-
     bot.activate()
