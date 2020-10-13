8991ed3b395736e0fc3f433147baab9cd97eb1b4
Kurokoi <driver2223@gmail.com>
2020-10-13 19:04:23 +0800

    format change and dota fix (#67)
    * fix image repeat
    
    * fix command repeat and discard func
    
    * format, debug-mode, fix set-name
    
    * add /setdota argument check
    
    * some minor fixs
    
    * fix dota command logic
    
    Co-authored-by: MMMartt <nielumartin@hotmail.com>

--- bot/logger.py
+++ bot/logger.py
@@ -5,25 +5,30 @@ from pathlib import Path
 
 
 class DefaultLogger(AbstractLogger):
+
     def __init__(self, level=logging.INFO, fmt_str=None):
         self.logger = logging.getLogger()
         self.logger.setLevel(level)
 
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(level)
-        
+        self.console_handler = logging.StreamHandler()
+        self.console_handler.setLevel(level)
+
         log_path = Path(__file__).parent.parent.joinpath(
             'logs', 'mirai_bot.log')
-        file_handler = TimedRotatingFileHandler(
-            log_path, when='h', interval=12, backupCount=5, encoding='utf-8')
-        file_handler.setLevel(level)
+        self.file_handler = TimedRotatingFileHandler(log_path,
+                                                     when='h',
+                                                     interval=12,
+                                                     backupCount=5,
+                                                     encoding='utf-8')
+        self.file_handler.setLevel(level)
 
         formatter = logging.Formatter(
-            fmt_str or "%(asctime)s - %(levelname)s: %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
-        file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
-        console_handler.setFormatter(formatter)
-        self.logger.addHandler(console_handler)
+            fmt_str or "%(asctime)s - %(levelname)s: %(message)s",
+            datefmt='%Y-%m-%d %H:%M:%S')
+        self.file_handler.setFormatter(formatter)
+        self.logger.addHandler(self.file_handler)
+        self.console_handler.setFormatter(formatter)
+        self.logger.addHandler(self.console_handler)
 
     def info(self, msg):
         msg = msg.replace("\n", r"\n")
@@ -40,5 +45,13 @@ class DefaultLogger(AbstractLogger):
 
     def exception(self, msg):
         self.logger.exception(msg, exc_info=True)
+
+    def close(self):
+        self.logger.removeHandler(self.console_handler)
+        self.logger.removeHandler(self.file_handler)
+        self.file_handler.close()
+        self.console_handler.close()
+        logging.shutdown()
+
 
 defaultLogger = DefaultLogger()
--- mods/repeat/__init__.py
+++ mods/repeat/__init__.py
@@ -5,29 +5,46 @@ from graia.application.friend import Friend
 from graia.application.message.chain import MessageChain
 from bot import Bot
 
-
-repeat_queue = [None,None]
+repeat_queue = [None, None]
 repeat_log = [None]
 sb_repeat_content = None
 
-async def repeat_handler(bot: Bot, message: MessageChain, subject: T.Union[Member, Friend]):
-    #复读添加群订阅机制
+
+def is_equal(a: MessageChain, b: MessageChain):
+    if not a or not b:
+        return False
+    l = len(a.__root__)
+    if l != len(b.__root__):
+        return False
+    for i in range(l):
+        if a.__root__[i].asSerializationString(
+        ) != b.__root__[i].asSerializationString():
+            return False
+    return True
+
+
+async def repeat_handler(bot: Bot, message: MessageChain,
+                         subject: T.Union[Member, Friend]):
+    # 复读添加群订阅机制
     global repeat_queue, sb_repeat_content
     pattern = r"^\s*\S{2,6}[SNsn][Bb][!！?？.。]{0,10}\s*$"
     message = message.asSendable()
     message_str = message.asDisplay()
-    if re.match(pattern, message_str):
-        if sb_repeat_content != message:
+    if re.match(pattern, message_str) and message_str[0] != bot.prefix:
+        if not is_equal(sb_repeat_content, message):
             sb_repeat_content = message
-            await bot.sendMessage(subject.group,message)
+            await bot.sendMessage(subject.group, message)
         return
     else:
         sb_repeat_content = message
-    if message_str[0]!=bot.prefix and message == repeat_queue[0] and message != repeat_log[0] and subject != repeat_queue[1]:
-        await bot.sendMessage(subject.group,message)
-        repeat_queue=[None,None]
+    if message_str[0] != bot.prefix and is_equal(
+            message, repeat_queue[0]) and not is_equal(
+                message, repeat_log[0]) and subject != repeat_queue[1]:
+        await bot.sendMessage(subject.group, message)
+        repeat_queue = [None, None]
         repeat_log[0] = message
     else:
-        repeat_queue = [message,subject]
+        repeat_queue = [message, subject]
 
-DIRECTS = {"repeat":repeat_handler}
+
+DIRECTS = {"repeat": repeat_handler}
