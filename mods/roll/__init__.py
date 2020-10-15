cff6fa48871e641f24c67e5863d8b668bc11019a
NekodRider <driver2223@gmail.com>
2020-10-16 00:03:47 +0800

    add simple recall

--- mods/roll/__init__.py
+++ mods/roll/__init__.py
@@ -19,7 +19,7 @@ async def roll_handler(*args, subject: T.Union[Member, Friend]):
         res = re.match(r"(\d+d\d+)(\+\d+)*", arg)
         if not res:
             return MessageChain.create([Plain("参数格式不对 请输入 2d6+4 形式")])
-        d, p = res[1], res[2]
+        d, p = res[1], res[2]   
         param = [int(x) if x != '' else 0 for x in d.split("d")]
         if param[0] > 100 or param[1] > 999999 or 0 in param:
             msg.append(Plain(f"{param[0]}d{param[1]} 参数为0或过大\n"))
@@ -31,7 +31,6 @@ async def roll_handler(*args, subject: T.Union[Member, Friend]):
             ))
 
     return MessageChain.create(msg)
-
 
 
 COMMANDS = {"roll": roll_handler}
